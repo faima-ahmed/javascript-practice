988357b497b0187376decf54dc8089171dd4f235
faima-ahmed <faimaahmeddiya@gmai.com>
2024-06-13 20:56:55 +0600

    test

---
+++ basics/object.js
@@ -0,0 +1,16 @@
+const myself={
+    firstName:'Faima',
+    lastName:'Diya',
+    age : 2002,
+    job: 'teacher',
+    friends: ['A', 'B', 'C', 'D']
+};
+
+console.log(myself);
+console.log(myself.lastName);
+console.log(myself['firstName']);
+
+const demo= prompt('what is your name');
+console.log(demo);
+
+//dot vs. bracket nation
