253c1715c318061175396609c6b7d132469bfe35
faima-ahmed <faimaahmeddiya@gmail.com>
2024-06-28 16:15:57 +0600

    wonderfull-code

---
+++ basics/for-loop.js
@@ -0,0 +1,35 @@
+// console.log('Lifting weights repetition 1 👩‍🦰');
+// console.log('Lifting weights repetition 2 👩‍🦰');
+// console.log('Lifting weights repetition 3 👩‍🦰');
+// console.log('Lifting weights repetition 4 👩‍🦰');
+// console.log('Lifting weights repetition 5 👩‍🦰');
+// console.log('Lifting weights repetition 6 👩‍🦰');
+// console.log('Lifting weights repetition 7 👩‍🦰');
+// console.log('Lifting weights repetition 8 👩‍🦰');
+// console.log('Lifting weights repetition 9 👩‍🦰');
+// console.log('Lifting weights repetition 10 👩‍🦰');
+
+//for-loop
+
+for(let rep=1; rep<=10; rep++){
+    //console.log(`Lifting weights repetition ${rep} 👩‍🦰`);
+}
+
+//looping arrays
+const myself=[
+    'Faima',
+    'Diya',
+    2037-2002,
+    'freelancer',
+    ['Faria', 'Khalid', 'Usha'],
+    true
+
+];
+
+// console.log(myself[0]);
+// console.log(myself[1]);
+// console.log(myself[3]);
+
+for(let i=0; i<myself.length; i++){
+    console.log(myself[i]);
+}
