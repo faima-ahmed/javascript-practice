ab435f6be093fc012b0cb8d07227e89243026a02
faima-ahmed <faimaahmeddiya@gmail.com>
2024-07-04 22:16:05 +0600

    the-game-logic

--- basics/Handling_Events.js
+++ basics/Handling_Events.js
@@ -1,12 +1,33 @@
 //random number
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent= number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score=20;
+document.querySelector('.number').textContent= secretNumber;
 
 document.querySelector('.check').addEventListener('click', function() {
-    const guess= document.querySelector('.guess').value;
-    console.log(guess);
+    const guess=Number (document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
 
     if(!guess){
         document.querySelector('.message').textContent='⛔ No number!';
+    }
+    else if(guess === secretNumber){
+        document.querySelector('.message').textContent='🎉 Correct Number!';
+    }
+    else if(guess > secretNumber){ 
+        if(score > 0) {
+            document.querySelector('.message').textContent='📈 Too high!';
+        score--;
+        document.querySelector('.score').textContent= score;
+        }
+
+        else{
+            document.querySelector('.message').textContent='🎇 You lost the game!';
+        }
+        
+    }
+    else if(guess < secretNumber){
+        document.querySelector('.message').textContent='📉 Too low!';
+        score--;
+        document.querySelector('.score').textContent= score;
     }
 })
