19530ea641a5a496db6adaf90fc93239a2fe1213
faima-ahmed <faimaahmeddiya@gmail.com>
2024-07-09 22:55:37 +0600

    or-operator

---
+++ basics/short-circuiting.js
@@ -0,0 +1,17 @@
+/* or operator
+true || true == true
+true || false == true
+false || true == true
+false || false == false */
+
+let firstNum= 3;
+let secondNum= 28;
+
+if(firstNum>5 || secondNum<25){
+    console.log('Greather than 5 or less than 25');
+}
+
+else{
+    console.log('another things');
+}
+
