0aba68df393e59dc59f7d48c62e3ecc6fd088e39
faima-ahmed <faimaahmeddiya@gmail.com>
2024-07-10 22:42:47 +0600

    and-operator

---
+++ basics/and-operator.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*
+true && false === false
+true && true === true
+false && false === false
+false && true === false
+*/ 
+
+let firstNum= 10;
+let secondNum= 25;
+
+if((firstNum>5) && (secondNum<30)){
+    console.log('The Number is currect');
+}
+
+else{
+    console.log('The Number is wrong');
+}
