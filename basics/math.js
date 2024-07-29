69c7943519b0623726a5961cd84bbcfbf573a7f9
faima-ahmed <faimaahmeddiya@gmail.com>
2024-07-29 07:44:20 +0600

    Commit 1

--- basics/math.js
+++ basics/math.js
@@ -12,5 +12,9 @@ console.log(Math.PI * Number.parseFloat('10px') ** 2);
 console.log(Math.trunc(Math.random()*6) + 1);
 
 const randomInt=(min, max) =>
-    Math.trunc(Math.random()* (max-min) + 1) + min;
+    Math.floor(Math.random()* (max-min) + 1) + min;
 console.log(randomInt(10,20));
+
+//rounding integers
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
