8f7f17ad31cdf5e29de6216b94e5656a9013eb2c
faima-ahmed <faimaahmeddiya@gmail.com>
2024-08-05 09:59:12 +0600

    Commit 1

--- basics/remainder-number.js
+++ basics/remainder-number.js
@@ -3,3 +3,9 @@ console.log(5 / 2);
 
 console.log(8 % 3);
 console.log(8 / 3);
+
+//even number
+const isEven= n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
