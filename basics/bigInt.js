4daecc339fb1bbca904d31e3897e01da509e2c2b
faima-ahmed <faimaahmeddiya@gmail.com>
2024-08-07 10:58:12 +0600

    Commit 1

---
+++ basics/bigInt.js
@@ -0,0 +1,8 @@
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 + 1);
+console.log(2 ** 53 + 2);
+console.log(2 ** 53 + 3);
+console.log(2 ** 53 + 4);
+console.log(2 ** 53 + 5);
+console.log(2 ** 53 + 6);
