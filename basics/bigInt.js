b353e343f57b0c4045e94f298fe4c9141e65a215
faima-ahmed <faimaahmeddiya@gmail.com>
2024-08-07 11:01:12 +0600

    Commit 1

--- basics/bigInt.js
+++ basics/bigInt.js
@@ -6,3 +6,9 @@ console.log(2 ** 53 + 3);
 console.log(2 ** 53 + 4);
 console.log(2 ** 53 + 5);
 console.log(2 ** 53 + 6);
+
+//exceptions
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof(20n));
+console.log(20n == '20');
