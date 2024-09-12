9f904b257892330303c76a911b9f6a31cf026e34
faima-ahmed <faimaahmeddiya@gmail.com>
2024-09-12 06:27:12 +0600

    Commit 1

--- basics/converting-number.js
+++ basics/converting-number.js
@@ -6,3 +6,10 @@ console.log(0.1+0.2 === 0.3);
 //conversion
 console.log(Number('23'));
 console.log(+"23");
+
+//parsing
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e23', 10));
+
+console.log(Number.parseInt('2.5rem'));
+console.log(Number.parseFloat('2.5rem'));
