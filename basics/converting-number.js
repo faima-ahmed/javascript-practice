eec1471e8832640c0d6813a8eef7305a68285f3c
faima-ahmed <faimaahmeddiya@gmail.com>
2024-09-13 06:12:13 +0600

    Commit 1

--- basics/converting-number.js
+++ basics/converting-number.js
@@ -13,3 +13,9 @@ console.log(Number.parseInt('e23', 10));
 
 console.log(Number.parseInt('2.5rem'));
 console.log(Number.parseFloat('2.5rem'));
+
+//check if value is not a number
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20X'));
+console.log(Number.isNaN(23 / 0));
