cacfa4298fe2c119e2c2d3caf1556e837009e953
faima-ahmed <faimaahmeddiya@gmail.com>
2024-09-15 07:15:13 +0600

    Commit 1

--- basics/math.js
+++ basics/math.js
@@ -3,3 +3,7 @@ console.log(25 ** (1/2));
 console.log(8 ** (1/3));
 
 console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2));
+console.log(Math.max(5, 18, '23px', 11, 2));
+
+console.log(Math.min(5, 18, 23, 11, 2));
