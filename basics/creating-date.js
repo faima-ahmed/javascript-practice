d30402b2bb7205f876c604ee25b0a750ccf5ac56
faima-ahmed <faimaahmeddiya@gmail.com>
2024-09-19 16:55:07 +0600

    date

---
+++ basics/creating-date.js
@@ -0,0 +1,25 @@
+//creating date
+/*
+const now = new Date();
+console.log(now);
+
+console.log(new Date('Thu Sep 19 2024 16:24:04'));
+console.log(new Date('December 24, 2015'));
+//console.log(account1.movementDates[0]);
+
+console.log(new Date(2037, 10, 19, 15, 23, 5));
+console.log(new Date(2037, 10, 31));
+
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+*/
+
+const future= new Date(2024, 10, 19, 15, 23);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
